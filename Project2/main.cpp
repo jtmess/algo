026fe55fa31d30dde7c6f86f34c6f0d9d39ddfc7
Jack Magrath <jackmagrath@jacks-macbook-pro.local>
2017-02-07 22:46:31 -0500

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Jack Magrath on 2/7/17.
+//  Copyright © 2017 Jack Magrath. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
